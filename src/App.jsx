1ec63c303374d97db2373ebaee4b9bd6918f9469
Nafis-Sahriar <nafis.apid@gmail.com>
2026-03-29 19:48:33 +0600

    Bismillahir Rahmanir Rahim - Project Initiated With All Necessary Files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import NavBar from "./Components/Navbar/NavBar"
+
+
+function App() 
+{
+  
+
+  return (
+    <>
+      <NavBar></NavBar>
+
+ 
+
+ 
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/NavBar.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { CiShoppingCart } from "react-icons/ci";
+import digiLogo from "../../assets/DigiTools.png";
+import { AiOutlineMenuFold } from "react-icons/ai";
+
+const NavBar = () => {
+    return (
+        <div className="navbar bg-base-100 shadow-sm md:w-[90%] mx-auto">
+
+             <div className="navbar-start">
+                <div className="dropdown">
+                      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg> */}
+                        <AiOutlineMenuFold />
+                     </div>
+                  <ul  tabIndex="-1" className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                       <li><a>Products</a></li>
+                       <li><a>Features</a></li>
+                       <li><a>Pricing</a></li>
+                       <li><a>Testimonials</a></li>
+                       <li><a>FAQ</a></li>
+                 </ul>
+               </div>
+
+                <a className="btn btn-ghost text-xl"><img className='w-20 md:w-40' src={digiLogo} alt="" /></a>
+            </div>
+
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+
+       <li className='hover:bg-primary font-semibold hover:text-white hover:rounded-xl'><a>Products</a></li>
+       <li className='hover:bg-primary font-semibold hover:text-white hover:rounded-xl'><a>Features</a></li>
+       <li className='hover:bg-primary font-semibold hover:text-white hover:rounded-xl'><a>Pricing</a></li>
+       <li className='hover:bg-primary font-semibold hover:text-white hover:rounded-xl'><a>Testimonials</a></li>
+       <li className='hover:bg-primary font-semibold hover:text-white hover:rounded-xl'><a>FAQ</a></li>
+
+    </ul>
+  </div>
+
+  <div className="navbar-end gap-2">
+    <button className='btn rounded-full text-shadow-purple-800 text-2xl'><CiShoppingCart /></button>
+    <button className='btn btn-ghost'>Log In</button>
+    <a className="btn rounded-full btn-primary font-semibold">Get Started</a>
+  </div>
+</div>
+    
+    );
+};
+
+export default NavBar;
